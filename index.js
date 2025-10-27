6e46cd97e74d3a9231ecbadfc8361757daf4cdd1
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-10-27 22:25:40 +0200

    Add function to show menu in index.js

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ async function readVal(p) {
 	let res = await rl.question(p);
 	return res;
 }
+
+async function showMenu() {
+	const res = await readVal("1. Show available users \n2. Create a new user \n3. Delete user\n4. Exit \nChoose an option: ");
+	return res;
+}
