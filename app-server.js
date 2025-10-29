67146ab77aa27091207d6a133d117330ae11f0bd
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-10-29 06:28:33 +0200

    Implement API request handling in app-server.js
    - Modify server logic to differentiate between API and static file requests
    - Integrate dispatchRequest function to process API requests and return JSON responses
    - Maintain existing functionality for serving static files using serve-handler

--- app-server.js
+++ app-server.js
@@ -1,8 +1,21 @@
 import { createServer } from "http";
 import handler from "serve-handler";
+import { dispatchRequest } from "./api.js";
 
 const server = createServer(async (request, response) => {
-	await handler(request, response);
+	const url = request.url;
+	const api = "/api/"
+
+	if (url?.startsWith(api)) {
+		const requestPath = url.slice(url.indexOf(api) + api.length)
+
+		const json = await dispatchRequest(requestPath);
+		response.setHeader('Content-Type', 'application/json');
+		response.statusCode = 200;
+		response.end(json);
+	} else {
+		await handler(request, response);
+	}
 });
 
 const port = 5500;
