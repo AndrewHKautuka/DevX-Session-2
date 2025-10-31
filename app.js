b80c88dc9bd55f43fb8cf23e6772c9a422aec714
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-10-31 13:45:17 +0200

    Add create user functionality in app.js
    - Integrate create user button and event listener to trigger user creation
    - Implement createUser function to handle form submission and API interaction
    - Enhance user feedback with success and error messages upon user creation

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import { getCreateUserForm } from "./create-user-form.js";
 import { getUsersTable } from "./users-table.js";
 
 const contentDiv = document.getElementById("content");
 
 const showUsersButton = document.getElementById("button-show-users");
 showUsersButton.addEventListener("click", () => showUsers());
+
+const createUserButton = document.getElementById("button-create-user");
+createUserButton.addEventListener("click", () => createUser());
 
 async function showUsers() {
 	try {
@@ -22,6 +26,52 @@ async function showUsers() {
 		const usersTable = getUsersTable(users);
 
 		contentDiv.replaceChildren(userCountLabel, usersTable);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function createUser() {
+	try {
+		const label = document.createElement("p");
+		label.className = "form-label";
+
+		const postUser = async (userData) => {
+			let payload
+
+			if (userData instanceof FormData) {
+				payload = {};
+				for (const [key, value] of userData.entries()) {
+					if (payload[key]) {
+						// If key already exists, convert to array or push to existing array
+						if (!Array.isArray(payload[key])) {
+							payload[key] = [payload[key]];
+						}
+						payload[key].push(value);
+					} else {
+						payload[key] = value;
+					}
+				}
+			} else {
+				payload = userData
+			}
+
+			const response = await fetch("api/users", {
+				method: "POST",
+				body: JSON.stringify(payload),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+			if (!response.ok) {
+				label.textContent = "Failed to post user";
+			} else {
+				label.textContent = "Successfully created new user";
+			}
+		};
+
+		const createUserForm = getCreateUserForm(postUser);
+		contentDiv.replaceChildren(createUserForm, label);
 	} catch (error) {
 		console.log(error);
 	}
