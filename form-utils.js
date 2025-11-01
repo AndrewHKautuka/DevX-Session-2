4de3ca1651e7d7c5a410b136f104a4fb62c3a7c9
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-11-01 17:38:39 +0200

    Enhance form input handling with value support
    - Update createFormInputs to pass input values for date and regular inputs, improving user experience by retaining input data.
    - Modify addInput and addDateInput functions to accept and set default values, ensuring better form state management.

--- form-utils.js
+++ form-utils.js
@@ -1,4 +1,5 @@
 import { addLabel } from "./component-utils.js";
+import { reformatDateString } from "./date-utils.js";
 
 export function createFormInputs(id, inputs) {
 	const formInputs = document.createElement("div");
@@ -7,30 +8,37 @@ export function createFormInputs(id, inputs) {
 	inputs.forEach(input => {
 		addLabel(formInputs, input.id, input.label);
 		if (input.type === "date") {
-			addDateInput(formInputs, input.id);
+			addDateInput(formInputs, input.id, input.value);
 		} else {
-			addInput(formInputs, input.id, input.placeholder);
+			addInput(formInputs, input.id, input.placeholder, input.value);
 		}
 	});
 
 	return formInputs;
 }
 
-function addInput(parent, id, placeholder) {
+function addInput(parent, id, placeholder, value) {
 	const input = document.createElement("input");
 	input.id = id;
 	input.name = id;
 	input.placeholder = placeholder;
 	input.required = true;
+	if (value) {
+		input.value = value;
+	}
 	parent.appendChild(input);
 }
 
-function addDateInput(parent, id) {
+function addDateInput(parent, id, value) {
 	const input = document.createElement("input");
 	input.id = id;
 	input.name = id;
 	input.type = "date";
 	input.required = true;
+	const dateValue = reformatDateString(value);
+	if (dateValue) {
+		input.value = dateValue;
+	}
 	parent.appendChild(input);
 }
 
